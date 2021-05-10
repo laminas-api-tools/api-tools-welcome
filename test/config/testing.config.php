76581f03a98abb690689c8df79a43999d10fbbb5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:13:03 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/config/testing.config.php
+++ test/config/testing.config.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas-api-tools/api-tools-welcome for the canonical source repository
- * @copyright https://github.com/laminas-api-tools/api-tools-welcome/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas-api-tools/api-tools-welcome/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\ApiTools\Welcome;
 
 return [
